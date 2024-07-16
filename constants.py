6842dec34a2a6507da1ba005c80145ba41fd9c34
citydeer <citydeer@gmail.com>
2024-07-17 03:30:11 +0800

    Change data path.

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ import os
 
 ### Task parameters
 # DATA_DIR = '/home/zfu/interbotix_ws/src/act/data' if os.getlogin() == 'zfu' else '/scr/tonyzhao/datasets'
-DATA_DIR = '/home/citydeer/work/aloha/act-plus-plus/data'
+DATA_DIR = '/home/citydeer/ubuntu-20.04/aloha/data'
 SIM_TASK_CONFIGS = {
     'sim_transfer_cube_scripted':{
         'dataset_dir': DATA_DIR + '/sim_transfer_cube_scripted',
